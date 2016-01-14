dcb45dc7200cf5d4ef417244a9d04a9ce63c9479
mauricekoster <maurice.koster@nl.ccv.eu>
2016-01-14 14:40:50 +0100

    PEP8 compliant Spelling control

--- gherkin2robotframework.py
+++ gherkin2robotframework.py
@@ -1,32 +1,33 @@
 from gherkin3.parser import Parser
-import yaml
-import os, re
+import os
+import re
 import argparse
-import glob, fnmatch
+import fnmatch
 
 # - Commandline parsing -------------------------------------------------------
 
-cmdlineparser =  argparse.ArgumentParser()
-cmdlineparser.add_argument("feature", nargs="?", default="")
-cmdlineparser.add_argument("outputdir", nargs="?", default=".")
-cmdline_args = cmdlineparser.parse_args()
+cmdline_parser = argparse.ArgumentParser()
+cmdline_parser.add_argument("feature", nargs="?", default="")
+cmdline_parser.add_argument("output", nargs="?", default=".")
+cmdline_args = cmdline_parser.parse_args()
 
 # - Globals -------------------------------------------------------------------
 
 FIELD_SEP = "    "
 settings_lines = []
-testcases_lines = []
+test_cases_lines = []
 keywords_lines = []
 seen_steps = set([])
 
-def process_gherkin(gherkin_filename, basedir, outputdir):
+
+def process_gherkin(gherkin_filename, basedir, output):
     with open(gherkin_filename, 'r') as f:
-        str = f.read()
+        content = f.read()
     parser = Parser()
-    feature = parser.parse(str)
-    global settings_lines, testcases_lines, keywords_lines, seen_steps
+    feature = parser.parse(content)
+    global settings_lines, test_cases_lines, keywords_lines, seen_steps
     settings_lines = []
-    testcases_lines = []
+    test_cases_lines = []
     keywords_lines = []
     seen_steps = set([])
 
@@ -34,57 +35,61 @@ def process_gherkin(gherkin_filename, basedir, outputdir):
 
     feature_base = os.path.dirname(gherkin_filename)
     feature_sub = feature_base[len(basedir)+1:]
-    generate_robot_script(os.path.join(outputdir, feature_sub), feature['name'])
+    generate_robot_script(os.path.join(output, feature_sub), feature['name'])
 
-def writetoscript(outfile, line):
+
+def write_to_script(outfile, line):
     if type(line) is list:
         outfile.write(FIELD_SEP.join(line) + '\n')
     else:
         outfile.write(line + '\n')
 
-def generate_robot_script(path, featurename):
+
+def generate_robot_script(path, feature_name):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    stepdefinitions_resource = "%s_stepdefinitions.robot" % featurename
-    stepdefinitions_resource = stepdefinitions_resource.lower().replace(' ', '_')
+    step_definitions_resource = "%s_step_definitions.robot" % feature_name
+    step_definitions_resource = step_definitions_resource.lower().replace(' ', '_')
 
-    fn = featurename.lower().replace(' ', '_') + '.robot'
+    fn = feature_name.lower().replace(' ', '_') + '.robot'
     with open(os.path.join(path, fn), 'w') as outfile:
-        writetoscript(outfile, "*** Settings ***")
-        writetoscript(outfile, ["Resource", stepdefinitions_resource])
+        write_to_script(outfile, "*** Settings ***")
+        write_to_script(outfile, ["Resource", step_definitions_resource])
         for line in settings_lines:
-            writetoscript(outfile, line)
+            write_to_script(outfile, line)
 
-        writetoscript(outfile, '')
+        write_to_script(outfile, '')
 
-        writetoscript(outfile, "*** Test Cases ***")
-        for line in testcases_lines:
-            writetoscript(outfile, line)
-        writetoscript(outfile, '')
+        write_to_script(outfile, "*** Test Cases ***")
+        for line in test_cases_lines:
+            write_to_script(outfile, line)
+        write_to_script(outfile, '')
 
         if keywords_lines:
-            writetoscript(outfile, "*** Keywords ***")
+            write_to_script(outfile, "*** Keywords ***")
             for line in keywords_lines:
-                writetoscript(outfile, line)
-            writetoscript(outfile, '')
+                write_to_script(outfile, line)
+            write_to_script(outfile, '')
 
-    generate_robot_script_resource(path,stepdefinitions_resource)
+    generate_robot_script_resource(path, step_definitions_resource)
 
-def generate_robot_script_resource(path, stepdefinitions_resource):
-    fn = os.path.join(path, stepdefinitions_resource)
+
+def generate_robot_script_resource(path, step_definitions_resource):
+    fn = os.path.join(path, step_definitions_resource)
     if os.path.exists(fn):
-        print "existing file " + stepdefinitions_resource
+        print "existing file " + step_definitions_resource
     else:
-        print "new file " + stepdefinitions_resource
+        print "new file " + step_definitions_resource
         with open(fn, 'w') as f:
-            writetoscript(f, '*** Settings ***')
-            writetoscript(f, '*** Keywords ***')
+            write_to_script(f, '*** Settings ***')
+            write_to_script(f, '*** Keywords ***')
             for l in seen_steps:
-                writetoscript(f, l)
-                writetoscript(f, ['','No Operation'])
+                write_to_script(f, l)
+                write_to_script(f, ['', 'No Operation'])
 
     print seen_steps
+
 
 def process_feature(feature):
     global background_available
@@ -95,17 +100,19 @@ def process_feature(feature):
     for scenario in feature['scenarioDefinitions']:
         process_scenario(scenario)
 
+
 def process_background(feature):
     global background_available
     background_available = True
 
     keywords_lines.append('Background')
     if feature['background']['name']:
-        keywords_lines.append(['','[Documentation]',feature['background']['name']])
+        keywords_lines.append(['', '[Documentation]', feature['background']['name']])
 
     for step in feature['background']['steps']:
         add_step(keywords_lines, step)
     keywords_lines.append('')
+
 
 def process_scenario(scenario):
     if scenario['type'] == 'Scenario':
@@ -117,8 +124,9 @@ def process_scenario(scenario):
     else:
         print 'Unknown scenario step ' + scenario['name'] + '>' + scenario['type']
 
+
 def add_step(output, step):
-    text = step['text'].replace('<','${').replace('>','}')
+    text = step['text'].replace('<', '${').replace('>', '}')
     if step['keyword'] == '* ':
         keyword = text
     else:
@@ -126,19 +134,22 @@ def add_step(output, step):
     seen_steps.add(text)
     output.append(['', keyword])
 
-def process_scenario_plain(scenario):
-    testcases_lines.append(scenario['name'])
-    if background_available:
-        testcases_lines.append(['','Background'])
-    for step in scenario['steps']:
-        add_step(testcases_lines, step)
-    testcases_lines.append('')
 
-def emptyfi(x):
-    if x=='':
+def process_scenario_plain(scenario):
+    test_cases_lines.append(scenario['name'])
+    if background_available:
+        test_cases_lines.append(['', 'Background'])
+    for step in scenario['steps']:
+        add_step(test_cases_lines, step)
+    test_cases_lines.append('')
+
+
+def make_empty(x):
+    if x == '':
         return '${EMPTY}'
     else:
         return x
+
 
 def process_scenario_outline(scenario):
     # collect variables in steps
@@ -148,46 +159,45 @@ def process_scenario_outline(scenario):
         variables += v
     variables = set(variables)
 
-    example_data = {}
-
     # per example a test case
     for example in scenario['examples']:
         if example['name']:
-            testcasename = scenario['name'] + ': '+ example['name']
+            test_case_name = scenario['name'] + ': ' + example['name']
         else:
-            testcasename = scenario['name'] + ' example line ' + str(example['location']['line'])
+            test_case_name = scenario['name'] + ' example line ' + str(example['location']['line'])
 
-        testcases_lines.append(testcasename)
-        testcases_lines.append(['', '[Template]', 'Scenario Outline ' + scenario['name']])
+        test_cases_lines.append(test_case_name)
+        test_cases_lines.append(['', '[Template]', 'Scenario Outline ' + scenario['name']])
 
         header_col = {}
-        colnr = 0
+        col_nr = 0
+
         for header_cell in example['tableHeader']['cells']:
             v = header_cell['value']
-            header_col[v] = colnr
-            colnr += 1
-
+            header_col[v] = col_nr
+            col_nr += 1
 
         for example_row in example['tableBody']:
             args = []
             for a in variables:
                 args.append(example_row['cells'][header_col[a]]['value'])
 
-            args = map(emptyfi, args)
+            args = map(make_empty, args)
             args.insert(0, '')
-            testcases_lines.append(args)
+            test_cases_lines.append(args)
 
-        testcases_lines.append('')
+        test_cases_lines.append('')
 
     # Test Template
     keywords_lines.append('Scenario Outline ' + scenario['name'])
     arguments = ['${' + arg + '}' for arg in variables]
     keywords_lines.append(['', '[Arguments]'] + arguments)
     if background_available:
-        keywords_lines.append(['','Background'])
+        keywords_lines.append(['', 'Background'])
     for step in scenario['steps']:
         add_step(keywords_lines, step)
     keywords_lines.append('')
+
 
 def get_feature_filenames(feature_basedir):
     matches = []
@@ -196,16 +206,21 @@ def get_feature_filenames(feature_basedir):
             matches.append(os.path.join(root, filename))
     return matches
 
-def process_directory(d, outputdir):
+
+def process_directory(d, output_dir):
     l = get_feature_filenames(d)
     for f in l:
-        process_gherkin(f, d, outputdir)
+        process_gherkin(f, d, output_dir)
+
+# --------------------------------------------------------------------------------------
+# Main part
+# --------------------------------------------------------------------------------------
 
 if cmdline_args.feature:
     if os.path.isdir(cmdline_args.feature):
         # glob
-        process_directory(cmdline_args.feature, cmdline_args.outputdir)
+        process_directory(cmdline_args.feature, cmdline_args.output)
     else:
-        process_gherkin(cmdline_args.feature, '.', cmdline_args.outputdir)
+        process_gherkin(cmdline_args.feature, '.', cmdline_args.output)
 else:
     process_directory('.', '.')
