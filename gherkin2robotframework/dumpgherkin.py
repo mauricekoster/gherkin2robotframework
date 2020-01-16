164db76139b4184052ec97f3cba832522f823b85
Maurice Koster <maurice@mauricekoster.com>
2020-01-16 21:45:26 +0100

    Setup for PyPI

---
+++ gherkin2robotframework/dumpgherkin.py
@@ -0,0 +1,23 @@
+from gherkin3.parser import Parser
+import yaml
+import argparse
+
+
+def dump_gherkin(gherkin_filename):
+    with open(gherkin_filename, 'r') as f:
+        content = f.read()
+    parser = Parser()
+    feature = parser.parse(content)
+
+    print(yaml.dump(feature))
+
+
+def main():
+    cmdlineparser = argparse.ArgumentParser()
+    cmdlineparser.add_argument("feature")
+    cmdline_args = cmdlineparser.parse_args()
+    dump_gherkin(cmdline_args.feature)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gherkin2robotframework",
+    version="0.1",
+    packages=["gherkin2robotframework"],
+    install_requires=["gherkin3==3.1.2", "pyyaml"],
+
+    author="Maurice Koster",
+    author_email="maurice@mauricekoster.com",
+    description="Translate Gherkin feature files into RobotFramework tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="gherkin robotframework",
+    url="https://github.com/mauricekoster/gherkin2robotframework/",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+    entry_points={
+        "console_scripts": [
+            "gherkin2robotframework = gherkin2robotframework.__main__:main",
+            "dumpgherkin = gherkin2robotframework.dumpgherkin:main",
+        ],
+    }
+)
